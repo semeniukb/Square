6582e560066b371af6fbe298b2236aa24aa7d4cb
semeniukb <crimsssz@gmail.com>
2022-08-14 20:29:54 +0300

    done

--- Square.js
+++ Square.js
@@ -6,8 +6,6 @@ function changeSquareColor (min, max) {
     return `rgb(${color.join(',')})`;
 }
 
-console.log(changeSquareColor(0, 256));
-
 document.body.addEventListener('click', () => {
     document.querySelector("#square").style.setProperty('border-color', `${changeSquareColor(0, 256)}`);
   });
